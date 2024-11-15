97d3f9f6f13e9d281689848652c0197217734ebe
hacker15243 <goyal.aadhya123@gmail.com>
2024-11-14 22:42:56 -0800

    End game works

--- Assets/Logic.cs
+++ Assets/Logic.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Logic : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    public int score = 0;
+    public GameObject gameOverScreen;
+
+    public void restartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    public void gameOver(){
+        gameOverScreen.SetActive(true);
     }
 }
