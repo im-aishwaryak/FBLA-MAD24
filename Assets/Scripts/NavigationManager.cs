7dfd10ae2e314328479f13c7e35f04ff900e8b0c
Ar2095 <181017654+aanyarawal21@users.noreply.github.com>
2025-04-19 09:58:39 -0700

    home screen

---
+++ Assets/Scripts/NavigationManager.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class NavigationManager : MonoBehaviour
+{
+    public void GoToProfile()
+    {
+        Debug.Log("working"); 
+        SceneManager.LoadScene("ProfileScreen"); 
+    }
+
+    public void GoToTrail()
+    {
+        SceneManager.LoadScene("Quiz"); 
+    }
+
+    public void GoToShop()
+    {
+        SceneManager.LoadScene("ShopScreen"); 
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); 
+    }
+}
