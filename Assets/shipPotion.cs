acb9e192520752c5157c68da8cb0b261791f8385
hacker15243 <goyal.aadhya123@gmail.com>
2025-06-26 12:50:44 -0700

    shipping works

--- Assets/shipPotion.cs
+++ Assets/shipPotion.cs
@@ -7,10 +7,18 @@ public class shipPotion : MonoBehaviour, IPointerClickHandler
 {
     // Start is called before the first frame update
     public PotionGameScript logic;
+
+    void Start()
+    {
+        // logic = GetComponent<PotionGameScript>();
+        Debug.Log("Initialized");
+        logic = GameObject.Find("LogicObjectName").GetComponent<PotionGameScript>();
+        
+    }
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log(this.name);
+        Debug.Log("is logic null: " + logic);
         logic.shipPotion();
-        
     }
 }
