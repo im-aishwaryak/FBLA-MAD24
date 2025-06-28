ae1c2fdf5d0b94ee1150d36d6cb0a3e06aece913
hacker15243 <goyal.aadhya123@gmail.com>
2025-06-28 08:32:01 -0700

    update running game

--- Assets/groundSpawnerScript.cs
+++ Assets/groundSpawnerScript.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+// using System.Numerics;
 using UnityEngine;
 
 public class groundSpawnerScript : MonoBehaviour
 {
     public GameObject ground;
     private float timer = 0;
-    private float spawnRate = 4;
+    private float spawnRate = 6.305f;
     // Start is called before the first frame update
     void Start()
     {
-        spawnGround();
+        spawnGround(new Vector3(0, -3.9576f, -5f));
+        spawnGround(new Vector3(25.22f, -3.9576f, -5f));
+        spawnGround(new Vector3(50.44f, -3.9576f, -5f));
+        
+        // spawnGround(new Vector3(18.2f, -3.9576f, -5f));
     }
 
     // Update is called once per frame
@@ -26,9 +31,17 @@ public class groundSpawnerScript : MonoBehaviour
             timer = 0;
         }
     }
-    
-    void spawnGround(){
-        Instantiate(ground, transform.position, transform.rotation);
+
+    void spawnGround()
+    {
+        Instantiate(ground, new Vector3(50.44f,-3.9576f, -5f), transform.rotation);
         // Debug.Log("Spawn Thing");
+    }
+
+    void spawnGround(Vector3 pos)
+    {
+        /*GameObject temp = */Instantiate(ground, pos, transform.rotation);
+        // SpriteRenderer sr = temp.GetComponent<SpriteRenderer>();
+        // Debug.Log(sr.bounds.size);
     }
 }
